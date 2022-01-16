8bbc62f4ae19bc013918f4cd0babc6f5da074717
Valentine <68567428+Valentime123@users.noreply.github.com>
2022-01-16 08:53:11 +0300

    Add files via upload

---
+++ Иванов Валентин(1).cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Arcane1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество белок");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Теперь введите количество орехов");
+            int K = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ответ:");
+            Console.WriteLine(K/N);
+
+        }
+    }
+}
